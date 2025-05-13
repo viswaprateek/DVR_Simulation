6b4430396ca622c9736857767ed73852c149a812
viswaprateek248 <viswaprateek248@gmail.com>
2025-05-13 21:57:05 +0530

    env fix

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
 // src/config.js
-const BACKEND_URL = "http://127.0.0.1:5000"; // Assuming Flask runs on port 5000
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://127.0.0.1:500';
 export default BACKEND_URL;
